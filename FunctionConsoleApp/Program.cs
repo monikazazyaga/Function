01580ab4d14e2cff5962ca8ac11526dafe4429ee
vitmeroa <vitmeroa@206D-6>
2024-02-12 14:36:15 +0700

    Добавьте файлы проекта.

---
+++ FunctionConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FunctionLibrary;
+
+namespace FunctionConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение k:");
+            double k = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение N:");
+            double N = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите количество итераций:");
+            int iterations = Convert.ToInt32(Console.ReadLine());
+
+            FunctionLibrary.Function function = new FunctionLibrary.Function();
+            function.CalculateFunction(k, N, iterations);
+        }
+    }
+}
---
+++ FunctionLibrary/Function.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionLibrary
+{
+    public class Function
+    {
+        public void CalculateFunction(double k, double N, int iterations)
+        {
+            try
+            {
+                int currentIteration = 0;
+
+                for (double x = 0; x <= k+1; x += 0.1)
+                {
+                    double result = 0.0;
+
+                    if (Math.Abs(x - N) == 0)
+                    {
+                        throw new DivideByZeroException("Ошибка: Деление на ноль");
+                    }
+                    else
+                    { 
+                        result = 1 / (x - N);
+                        if (x>k && currentIteration < iterations)
+                        {
+                            throw new OverflowException("Ошибка: Переполнение диапазона");
+                        }
+                      
+                        Console.WriteLine($"При x = {x:F1}, f(x) = {result:F2}");
+                    }
+
+                    currentIteration++;
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Конец программы");
+            }
+        }
+    }
+}
